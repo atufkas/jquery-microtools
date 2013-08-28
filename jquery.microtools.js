1d5438c44e0d41e667ad11d2cddbc320d0e77503
Matthias Lienau <matthias@mlienau.de>
2013-08-28 22:29:45 +0200

    Initial version.

---
+++ jquery.microtools.js
@@ -0,0 +1,33 @@
+(function($) {
+  
+  function isOverflown(e,axis) {
+    var ox = e.scrollWidth > e.clientWidth + 20;
+    var oy = e.scrollHeight > e.clientHeight;
+    
+    switch(axis) {
+      case 'x':
+        return ox;
+      case 'y':
+        return oy;
+      default:
+        return ox||oy;
+    }
+  }
+  
+  var pseudos = {
+		'mt-overflow': function(elm) {
+			return isOverflown(elm);
+		},
+		
+		'mt-overflow-x': function(elm) {
+			return isOverflown(elm,'x');
+		},
+		
+		'mt-overflow-y': function(elm) {
+			return isOverflown(elm,'y');
+		}
+	};
+	
+	$.extend($.expr[':'], pseudos);
+  
+})(jQuery);
